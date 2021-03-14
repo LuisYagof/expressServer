215070fd08d22a606e8e1480c7fb18554739cd25
LuisYagof <luisyagofdez@gmail.com>
2021-03-14 21:17:00 +0100

    feat: firebase realtime database storaging

--- expressServer.js
+++ expressServer.js
@@ -10,22 +10,39 @@ server.use(staticFilesPath);
 server.use(express.urlencoded({extended:false}));
 server.use(express.json());
 
+// ------------------FIREBASE--------------------
 
-// -----------------API REST-------------------
+const admin = require("firebase-admin");
+const serviceAccount = require("/Users/Luis-Dell/Documents/Estudios/Coding/BOOTCAMP/Semana7/express/express-init-firebase-adminsdk-6mdt1-1c7082f64a.json");
 
-const words = [ "Express", "Node", "React", "Mongo", "SQL"];
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://express-init-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
+const db = admin.database();
+// const ref = db.ref("restricted_access/secret_document");
+// ref.once("value", function(snapshot) {
+//   console.log(snapshot.val());
+// });
 
 // ------------PETICIÓN DE LECTURA--------------
 
+
 server.get('/words', (req, res) => {
-    res.send(words)
+    db.ref('words')
+    .on('value', (response) => {
+        res.send(response.val())
+        // let data = response.val()
+        // next = data.length
+    })
 })
 
 // -----------PETICIÓN DE ESCRITURA-------------
 
 server.post('/words', (req, res) => {
-    words.push(req.body.neword)
+    db.ref('words/5')
+        .set(req.body.neword)
     res.send()
 })
 
